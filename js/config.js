513d2ac2406ff42525c65a376dac6feeef476511
DarkMoon912 <86262929+DarkMoon912@users.noreply.github.com>
2022-06-11 02:48:44 +0700

    epic

---
+++ js/config.js
@@ -0,0 +1,44 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 2.5,
+
+  // Заголовок
+  title: 'DarKi#1288',
+
+  // Социальные сети
+  social: {
+    discord:'zaynehouse',
+    telegram: '@DarkMooon83',
+  },
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#198bfa',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#198bfa',
+  // Цвет тени кнопки play
+  playButtonShadowColor: '#90cbff',
+
+
+  // Цвет заголовка
+  titleColor: '#198bfa',
+  // Цвет тени загаловка
+  titleShadowColor: '#90cbff',
+
+
+  // Текст копирайта
+  copyrightText: 'dev - DarKi#1288',
+  // Цвет копирайта
+  copyrightTextColor: '#198bfa',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#198bfa',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: '#90cbff',
+}
